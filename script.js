672477aff5d8e8712ce2c34715da95a7836cb16f
server <erikdahlqvist@protonmail.com>
2022-06-12 15:05:37 +0200

    Added script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+var selectedColor = "white"
+
+function selectColor(color) {
+	selectedColor = color;
+}
+
+function initStats(states) {
+	console.log(states);
+	states.forEach(state => {
+
+		document.getElementById("stateMap").contentDocument.getElementById(state).addEventListener(
+			"click",
+			function() {
+				console.log(`Clicked on ${state}`)
+				this.style.fill = selectedColor;
+		});
+	});
+	
+}
+
+function fetchData() {
+	fetch('/electoralmap/states.json')
+		.then(data => data.json())
+		.then(success => initStats(success))
+		.catch(error => console.error('There has been a problem with your fetch operation:', error));
+}
